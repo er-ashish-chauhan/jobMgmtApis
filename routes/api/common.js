986aaecdc2463db57a364422d1223b423dc2b75b
Ashish Chauhan <imashish58@gmail.com>
2021-11-28 12:24:31 +0530

    apis

--- routes/api/common.js
+++ routes/api/common.js
@@ -12,31 +12,83 @@ const { query, check, validationResult } = require("express-validator");
 const connection = require("../../config/connection");
 const { root } = require('../../general')
 
-// @route    GET api/common
+// @route    GET api/common/firms
 // @desc     Get firms listing
 // @access   Public
 router.get(
     "/firms",
-    //   query("userId", "Please include a valid userId").exists(),
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        // const { userId } = req.query;
+        const { searchText } = req.query;
 
         try {
-            // Get jobs
+
+            const queryForExecute = searchText ? 'SELECT * FROM firm Where firm_name like "%' + searchText + '%"'
+                : `SELECT * FROM firm`
+
+            // Get firms
             connection.execute(
-                `SELECT * FROM firm`,
+                queryForExecute,
                 async (err, rows, fields) => {
                     if (err) {
                         console.error(err);
                         throw err;
                     }
                     if (rows.length) {
-                        res.json(rows)
+                        const result = {
+                            status: res.statusCode,
+                            data: {
+                                firms: rows,
+                                totalRecords: rows.length
+                            },
+                        }
+                        res.json(result)
+                    } else {
+                        return res
+                            .status(401)
+                            .json({ message: "No records found" });
+                    }
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        }
+    }
+);
+
+// @route    GET api/common/commodities
+// @desc     Get firms listing
+// @access   Public
+router.get(
+    "/commodities",
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            // Get commodities
+            connection.execute(
+                `SELECT * FROM commodities`,
+                async (err, rows, fields) => {
+                    if (err) {
+                        console.error(err);
+                        throw err;
+                    }
+                    if (rows.length) {
+                        const result = {
+                            status: res.statusCode,
+                            data: {
+                                commodities: rows,
+                                totalRecords: rows.length
+                            }
+                        }
+                        res.json(result)
                     } else {
                         return res
                             .status(401)
--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -2,42 +2,46 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
-const jwt = require("jsonwebtoken");``
+const jwt = require("jsonwebtoken"); ``
 const config = require("config");
 const formidable = require("formidable");
 const path = require('path')
 const fs = require('fs')
-const {query, check, validationResult } = require("express-validator");
+const { query, check, validationResult } = require("express-validator");
 
 const connection = require("../../config/connection");
-const {root} = require('../../general')
+const { root } = require('../../general')
 
 // @route    GET api/jobs
 // @desc     Get job listing by userId
 // @access   Public
 router.get(
-  "/",
-  query("userId", "Please include a valid userId").exists(),
+  "/getJobs",
+  query("firmId", "Please include a valid firmId").exists(),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      let errResponse = {
+        status: res.statusCode,
+        data: [],
+        error: errors.array()
+      }
+      return res.json(errResponse);
     }
 
-    const { userId } = req.query;
+    const { firmId } = req.query;
 
     try {
       // Get jobs
       connection.execute(
-        `SELECT job.id, job.job_name, job.assignToId, job.firmId,
-          job.total_quantity, job.net_quantity, job.pricePerQty, 
-          job.created, job.updated,
-          users.firstName, users.lastName,
-          firm.firm_name, firm.address
-          FROM job LEFT JOIN users ON job.assignToId = users.id
-          LEFT JOIN firm on job.firmId = firm.id
-          WHERE job.assignToId=?`,
-        [userId],
+        `SELECT job.id, job.job_name as jobName, job.firmId,
+          job.total_quantity, job.net_quantity, job.quantityType, job.dealValidFrom, job.dealValidUpto, 
+          job.deliveryType, job.status, job.price,
+          commodities.commodity, firm.firm_name as firmName, firm.address as firmAddress
+          FROM job LEFT JOIN firm on job.firmId = firm.id
+          LEFT JOIN commodities on commodities.id = job.commodityId
+          WHERE job.firmId=?`,
+        [firmId],
         async (err, rows, fields) => {
           if (err) {
             console.error(err);
@@ -45,12 +49,21 @@ router.get(
           }
 
           if (rows.length) {
-            res.json(rows)
-            
+            const result = {
+              status: res.statusCode,
+              data: {
+                jobs: rows,
+                totalsRecords: rows.length
+              }
+            }
+            res.json(result)
           } else {
-            return res
-              .status(401)
-              .json({ message: "No records found" });
+            let errResponse = {
+              status: res.statusCode,
+              data: [],
+              error: { msg: "No records found" }
+            }
+            return res.json(errResponse)
           }
         }
       );
@@ -69,50 +82,50 @@ router.post(
   "/:jobId",
   async (req, res) => {
     try {
-      
-    const { jobId } = req.params;
 
-    const form = formidable({
-      keepExtensions: true,
-      // maxFileSize: 15 * 1024 * 1024,
-      uploadDir: root + "/tmp",
-    });
-    form.parse(req, (err, fields, files) => {
-      let response = {};
-      // check for any error if err. then return with a message
-      if (err) {
-        response.status = "error";
-        response.msg = err.message;
-        return res.status(400).json(response);
-      }
-      // file upload code
-      const oldpath = files.image.path;
-      const ext = path.extname(files.image.path);
-      const file_name = "job_" + Date.now() + ext;
-      const newpath = path.join(root, "/uploads/") + file_name;
-        
-      // rename file to a upload folder
-      fs.rename(oldpath, newpath, async function (err) {
-        if (err) throw err;
+      const { jobId } = req.params;
 
-        connection.execute(
-          `INSERT INTO jobMeta
+      const form = formidable({
+        keepExtensions: true,
+        // maxFileSize: 15 * 1024 * 1024,
+        uploadDir: root + "/tmp",
+      });
+      form.parse(req, (err, fields, files) => {
+        let response = {};
+        // check for any error if err. then return with a message
+        if (err) {
+          response.status = "error";
+          response.msg = err.message;
+          return res.status(400).json(response);
+        }
+        // file upload code
+        const oldpath = files.image.path;
+        const ext = path.extname(files.image.path);
+        const file_name = "job_" + Date.now() + ext;
+        const newpath = path.join(root, "/uploads/") + file_name;
+
+        // rename file to a upload folder
+        fs.rename(oldpath, newpath, async function (err) {
+          if (err) throw err;
+
+          connection.execute(
+            `INSERT INTO jobMeta
             (jobId, image, quantityConfirmed)
             VALUES(?, ?, ?)`,
-          [jobId, newpath, fields.quantityConfirmed],
-          async (err, result) => {
-            if (err) {
-              console.error(err);
-              throw err;
-            }
-  
-           res.json({success: true})
-          }
-        );
+            [jobId, newpath, fields.quantityConfirmed],
+            async (err, result) => {
+              if (err) {
+                console.error(err);
+                throw err;
+              }
 
+              res.json({ success: true })
+            }
+          );
+
+        });
       });
-    });
-  
+
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
